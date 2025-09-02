4ce64649397dc07a1e5c16b63505c87762847bfe
Ayesha Fatima <ayeshaalifatima15@gmail.com>
2025-09-02 23:15:40 +0500

    Add files via upload
     Day-01 Digital Clock files

---
+++ script.js
@@ -0,0 +1,16 @@
+function clockUpdate() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+}
+setInterval(clockUpdate, 1000);
+
+function currentDate() {
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, "0");
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const year = now.getFullYear();
+    document.getElementById('date').textContent = `${day}/${month}/${year}`;
+}
