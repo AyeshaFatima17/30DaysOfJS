16d4a1a73b38481b7dc7292717c7d74a1a3a8f92
Ayesha Fatima <ayeshaalifatima15@gmail.com>
2025-09-12 22:36:31 +0500

    Form Validator day-10
    A basic form validation project that checks for empty fields, validates email format, and ensures password rules. Built using HTML, CSS, and JavaScript to demonstrate real-time validation.

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-const modal = document.getElementById("myModal");
-const btn = document.getElementById("openModalBtn");
-const span = document.getElementsByClassName("close")[0];
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+form.addEventListener('submit', (e) => {
+    let messages = [];
 
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (email.value.trim() === '' || email.value == null) {
+        messages.push('Email is required');
+    } else if (!/\S+@\S+\.\S+/.test(email.value)) {
+        messages.push('Email is not valid');
     }
-}
+
+    if (password.value.trim() === '' || password.value == null) {
+        messages.push('Password is required');
+    } else if (password.value.length <= 6) {
+        messages.push('Password must be longer than 6 characters');
+    } else if (password.value.length >= 20) {
+        messages.push('Password must be less than 20 characters');
+    } else if (password.value.toLowerCase() === 'password') {
+        messages.push('Password cannot be "password"');
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ');
+    } else {
+        errorElement.innerText = '';
+        alert("Form submitted successfully!");
+    }
+    });
